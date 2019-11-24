548227980cf294079dc10ed1091dd793ab16cb9b
Vo Minh Thu <thu@hypered.io>
2019-11-24 12:37:01 +0100

    First index.md version.
    I guess it is a bit pompous but I'm not sure how to sell a consultancy.

---
+++ build.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env bash
+
+set -e
+
+mkdir -p _site/fr/
+
+# Run Pandoc on source files using the design-system template.
+for i in \
+  index \
+  ; do \
+  echo Rendering $i.md...
+nix-shell -p pkgs.haskellPackages.pandoc --run "pandoc \
+  --standalone \
+  --template ../design-system/generated/templates/default.html \
+  --lua-filter ../design-system/pandoc/tachyons.lua \
+  --output _site/fr/$i.html \
+  fr/metadata.yml \
+  fr/$i.md"
+done
+
+rsync -aP _site/ docs/
---
+++ fr/index.md
@@ -0,0 +1,6 @@
+---
+title: Le développement logiciel, défini.
+---
+
+Hypered vous aide à définir une véritable vision technique et à la mettre en
+oeuvre en partageant sa culture du développement avec vos équipes.
